54c374403bf57bc1288570bffe50735b264e7be7
Zouhair Ettarak <ettarak.zouhair@gmail.com>
2022-01-19 22:02:50 +0100

    Retreive data from array employee

---
+++ src/app/components/employees/list-employee.component.ts
@@ -0,0 +1,55 @@
+import { Employee } from './../../models/employee';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-employee',
+  templateUrl: './list-employee.component.html',
+  styleUrls: ['./list-employee.component.css']
+})
+export class ListEmployeeComponent implements OnInit {
+
+  // how to retrieve this employees data from a database table
+  employees: Employee[] = [
+    {
+      id: 1,
+      name: 'Mark',
+      gender: 'Male',
+      contactPreference: 'Email',
+      email: 'mark@pragimtech.com',
+      dateOfBirth: new Date('10/25/1988'),
+      department: 'IT',
+      isActive: true,
+      photoPath: 'assets/images/mark.png'
+    },
+    {
+      id: 2,
+      name: 'Mary',
+      gender: 'Female',
+      contactPreference: 'Phone',
+      email: "mary@gmail.com",
+      phoneNumber: 2345978640,
+      dateOfBirth: new Date('11/20/1979'),
+      department: 'HR',
+      isActive: true,
+      photoPath: 'assets/images/mary.png'
+    },
+    {
+      id: 3,
+      name: 'John',
+      gender: 'Male',
+      contactPreference: 'Phone',
+      email: 'john@hotmail.fr',
+      phoneNumber: 5432978640,
+      dateOfBirth: new Date('3/25/1976'),
+      department: 'IT',
+      isActive: false,
+      photoPath: 'assets/images/john.png'
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
